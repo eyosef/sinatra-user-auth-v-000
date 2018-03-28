cb927b2e2d7a57f8e8acd054306864d75d1f659f
Eden Mesfun <emesfun@gmail.com>
2018-03-28 21:20:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,8 +29,9 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(email: params["email"], password: params["password"])
-    session[:id] = @user.id
+    if @user = User.find_by(email: params["email"], password: params["password"])
+      session[:id] = @user.id
+    end
     redirect '/users/home'
   end
 
